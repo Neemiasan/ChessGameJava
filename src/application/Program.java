295e3e859390c9e8c87e2aab8004b37a4a8604b0
Neemias Galvão Teixeira <neemias.tei@gmail.com>
2020-06-12 19:42:44 -0300

    Special

--- src/application/Program.java
+++ src/application/Program.java
@@ -29,14 +29,20 @@ public class Program {
 				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
 				UI.clearScreen();
 				UI.printBoard(chessMatch.getPieces(), possibleMoves);
-				
 				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
 				
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+				
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
+				}
+				
+				if (chessMatch.getPromoted() != null) {
+					System.out.print("Enter piece for promotion (B/N/R/Q): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
 				}
 			}
 			catch (ChessException e) {
@@ -48,9 +54,7 @@ public class Program {
 				sc.nextLine();
 			}
 		}
-		
 		UI.clearScreen();
 		UI.printMatch(chessMatch, captured);
-		
 	}
 }
